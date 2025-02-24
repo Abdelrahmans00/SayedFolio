14ba25bebb9d02550531889a218a25ad9d3155e6
Abdelrahmans00 <123298652+Abdelrahmans00@users.noreply.github.com>
2025-02-24 16:33:52 +0200

    deployed

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import Home from './Home';
 import NavBar from './NavBar';
 import Footer from './Footer';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React, { createContext, useEffect, useState } from 'react';
+import { HashRouter as Router, Routes, Route } from "react-router-dom"; // Using HashRouter
+import React, { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
 import Skills from './Skills';
@@ -11,34 +11,29 @@ import Projects from './Projects';
 import About from './About';
 import DarkMode from './DarkMode';
 
-
-
-
 const App = () => {
   useEffect(() => {
     AOS.init({ duration: 1000, once: true }); // Initialize AOS with options
   }, []);
 
-
-return (
-  <BrowserRouter>
-    <div className="App" >
-      <NavBar />
-      <DarkMode/>
-      <div className="container">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+  return (
+    <Router> {/* Changed from BrowserRouter to HashRouter */}
+      <div className="App">
+        <NavBar />
+        <DarkMode />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer/>
-    </div>
-  </BrowserRouter>
-);
+    </Router>
+  );
 }
-
 
 export default App;
